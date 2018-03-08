ed7fb61fc001c6e1a3b45f52e16834f9d5c563ba
master <1104-7@1104-7-PC>
2018-03-08 11:49:37 +0900

    마스터 join컨트롤러 추가

---
+++ src/mvc/controllers/JoinController.java
@@ -0,0 +1,14 @@
+package mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class JoinController {
+
+	@RequestMapping("/join")
+	public String accountJoinHandle() {
+		return "account/join";
+	}
+}
